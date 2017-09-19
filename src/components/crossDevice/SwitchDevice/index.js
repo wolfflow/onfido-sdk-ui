0453192b6582eee29f063eac428f7ababdc99719
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-09-19 17:03:51 +0100

    Fix infinite loading screen when uploading a document with glare

--- src/components/crossDevice/SwitchDevice/index.js
+++ src/components/crossDevice/SwitchDevice/index.js
@@ -1,19 +1,20 @@
 import { h } from 'preact'
+import {preventDefaultOnClick} from '../../utils'
 import style from './style.css'
 
-const MobileFlowOption = () => {
+const SwitchDevice = ({showMobileInstructions}) => {
   return (
     <div className={style.container}>
-      <a href='#'>
+      <a href='#' onClick={preventDefaultOnClick(showMobileInstructions)}>
         <div className={style.icon} />
         <div className={style.copy}>
           <div className={style.header}>Need to use your mobile to take photos?</div>
           <p className={style.submessage}>Safely continue verification on your mobile </p>
         </div>
+        <div className={style.chevron} />
       </a>
-      <div className={style.chevron} />
     </div>
   )
 }
 
-export default MobileFlowOption
+export default SwitchDevice
